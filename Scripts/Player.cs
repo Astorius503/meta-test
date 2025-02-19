f1ecd2eebee18859898b3bcfb6c8b53bb1ecdf57
Astorius503 <tiggh12@gmail.com>
2025-02-19 15:25:26 +0900

    [Feat] 이동 테스트

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.Experimental.RestService;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        float direction = 0.05f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        while (Application.isPlaying)
+        {
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                transform.position = Vector2.up;
+            }
+        }
     }
+
+   
 }
+
+ public class MoveP
+    {
+        public float x;
+        public float y;
+        public float PlayerD;
+
+
+    }
